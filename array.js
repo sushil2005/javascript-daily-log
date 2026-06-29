4fc05700b10b181ffd6d7310d4be1e6698ffdf8e
Sushil Gaire <sushil.241526@ncit.edu.np>
2026-06-29 16:45:58 +0545

    Add files via upload
    simple javascript concepts have been completed

---
+++ array.js
@@ -0,0 +1,31 @@
+let a=['apple','Banana','orange','pineapple',"china"];
+function compare(a,b)
+{
+    return a-b;
+}
+//document.write(a.sort(c));
+document.write(b[0]);
+document.write(a.reverse);
+//c=a.slice(1,3);
+
+ //c=a.splice(1,2,"cherry"); instead of use delete we use slice and splice in javascript
+document.write(c);
+//slice:it create a new element with element
+//document.write(a.includes('orange'));
+//document.write(a.join('@)'));
+//document.write(a.concat('strawberry'));
+
+
+//c=a.push("watermelon",'papaya');
+//d=a.pop();
+ //e=a.unshift("mango", "mulberry");
+//document.write(a,"<br>");
+//document.write(a.indexOf('banana'));
+
+//or let p=new array('apple','Banana','orange','pineapple')
+//document.write(a[3]);
+//a[3]="cherry";
+//document.write(a);
+//length
+//document.write(a.length);
+
---
+++ class4.js
@@ -0,0 +1,24 @@
+//constants
+//document.write(Math.PI);
+//document.write(Math.max(100,299,999,90,-10));
+//document.write(Math.round(100.5));
+//document.write(Math.floor(5.89));
+//document.write(Math.Truncate(10.5));
+//document.write(Math.abs(-3));
+//document.write(Math.sign(0));
+
+//document.write(Math.sqrt(34));
+//document.write("<br>");
+//document.write(Math.sqrt(4));
+//document.write(Math.trunc(Math.random()*99999))
+let n1=Number(prompt("enter the number"));
+for(i=1;i<=Number;i++)
+{
+ num=Math.floor(num/10);
+ count++;
+
+}
+
+document.write(Math.random()*n1*10);
+
+ 
