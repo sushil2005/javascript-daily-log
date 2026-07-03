00ca719c58c90011a400746a3f2306dfdac8a3d5
Sushil Gaire <sushil.241526@ncit.edu.np>
2026-07-03 22:31:44 +0545

    Add files via upload
    "Add JavaScript practice questions"

---
+++ class5.js
@@ -0,0 +1,11 @@
+let a="sushil is a good";
+//document.write(a.match("good"));
+//document.write(a.search("good"));
+
+// let a="simaaaaaaaaaaaaantaaaaaaaaa";
+//document.write(a.startsWith("s"));
+//document.write(a.endsWith("a"));
+//document.write(a.indexOf("a",9));
+
+
+
---
+++ class6.js
@@ -0,0 +1,77 @@
+//methods in DOM
+//getElementById
+ 
+//let a=document.getElementById("a");
+//console.log(a);
+
+//tagname
+//document.write(b.tagName);
+
+//innerText-> changes only text
+//document.write(b.innerText);
+///alert(b.innerText);
+//a.innerText="sushil gaire";
+
+
+//innerHTML-> changes text and html tags also
+//a.innerHTML="<u>hello</u>";
+//b.innerHTML="<mark>sushil gaire</mark>";
+//c.innerHTML="<i>sushil gaire</i>";
+ 
+//textContent-> used to display the hidden text
+//document.write(b.textContent);
+
+
+//src
+//let img=document.getElementById("p");
+//img.src="q.png";
+
+
+ 
+
+
+
+
+
+//getElementsByClassName
+//getElementsByTagName
+//querySelector
+ //let p-document.querySelector("li:nth-child(2)");
+ //for(i of p)
+    //i.style.backgroundColor="orange";
+ //let p=document.querySelector("#a");
+//p.style.backgroundColor="orange";
+
+
+//querySelectorAll
+
+//let pa=document.getElementsByClassName("c");
+//for(i of pa)
+//    i.style.backgroundColor="yellow";
+
+//for(i of p)
+
+//{
+    //i.style.backgroundColor="yellow";
+//}
+//c[1].style.backgroundColor="green";
+//c[2].style.backgroundColor="blue";
+
+
+//style
+ //cssText
+ //let he=document.getElementById("a");
+ //he.style.color="red";
+ //he.style.fontSize="30px";
+ //he.style.csstext="color:blue;font-size:30px;background-color:yellow";
+ //document.body.style.backgroundColor="pink";
+
+//let inp=document.getElementById("a");
+//alert(inp.value);
+//a.value="dont change";
+//b.placeholder="where are you going";
+
+
+
+//value
+//placeholder
