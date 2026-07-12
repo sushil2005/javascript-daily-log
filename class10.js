e00b468472a726c0c1d04ff86ef9cd29b3128a26
Sushil Gaire <sushil.241526@ncit.edu.np>
2026-07-12 23:54:25 +0545

    Add files via upload
    regular expression generating some questions have been practiced in js.

---
+++ class10.js
@@ -0,0 +1,21 @@
+let str="+977-97488219801";
+let pat=/^\+977\-9[7-8]\d{8}$/g;
+if(!pat.test(str))
+{
+    alert("milyo");
+}
+else
+{
+    alert("milena");
+}
+
+
+//let name="sushil is my name, I am  Am a studenT student student"
+//let pat=/am/gi;
+//console.log(pat.replace(pat,"nepal"));
+//console.log(pat.match(pat));
+//console.log(pat.search(name));
+//console.log(pat.test(pat));
+//console.log(pat.exec(name));
+//console.log(pat.exec(name));
+//console.log(pat.exec(name));
