2b9b4e32ab59609a76157574957a9b2eccf24697
Sushil Gaire <sushil.241526@ncit.edu.np>
2026-07-15 00:00:26 +0545

    Add files via upload
    some javascript questions are practiced

---
+++ lab 9-01.js
@@ -0,0 +1,11 @@
+x=20;
+document.write(x);
+
+let a=10;
+document.write(a);
+
+var b=35;
+document.write(b);
+
+const w=40;
+document.write(w);
---
+++ lab-09-04.js
@@ -0,0 +1,13 @@
+var number=Prompt("Enter a number");
+if(number==0)
+{
+    document.write(number +" is a zero");
+}
+else if(number>0)
+{
+    document.write(number +" is a positive number");
+}
+else
+{
+    document.write(number +" is a negative number");
+}
